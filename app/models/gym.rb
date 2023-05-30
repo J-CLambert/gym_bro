5fe99f9d40d476d0e7b9a692cf1594cc7cd1ea59
hiu lam tse <hiulam1@hotmail.com>
2023-05-30 15:57:01 +0200

    creating devise views

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,5 +2,5 @@ class Gym < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  validates :address, :description, presence: true
+  validates :title, :address, :description, presence: true
 end
