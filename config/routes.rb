07e31354171b5a8217691d0ca39311651a4acd2d
Lambert JC <jam@jumpyone.ch>
2023-05-30 17:18:06 +0200

    rien

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "gyms#index"
+  root to: "pages#home"
   resources :gyms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
