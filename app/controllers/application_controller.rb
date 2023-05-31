620d0a0f42349e303745c6e757e19065734bb475
hiu lam tse <hiulam1@hotmail.com>
2023-05-31 15:28:23 +0200

    buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     p resource
     stored_location_for(resource) || gyms_path
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
