57118d8c213c6a9d8b3ad0d23273d7db84b1af46
hiu lam tse <hiulam1@hotmail.com>
2023-06-02 10:40:48 +0200

    addedform on gym#show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :gym
   # validates :duration, presence: true, numericality: { only_integer: true }
   validates :user_id, :gym_id, presence: true
-  # validates :start_at, presence: true
+  #validates :start_at, presence: true
   enum status: { pending_confirmation: 0, confirmed: 1, training_ended: 2 }
 end
