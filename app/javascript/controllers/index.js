8739f752f26b6850e8bad9cefaf7a6324da4d8a7
hiu lam tse <hiulam1@hotmail.com>
2023-06-02 16:01:18 +0200

    index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,6 +3,11 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
